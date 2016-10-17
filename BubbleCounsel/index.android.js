87ac2e0941b33be4e5994a4ec46d3324f2c8647c
Michelle Tan <michelletanjm@gmail.com>
2016-10-17 12:53:34 +0800

    Clean index.js

--- BubbleCounsel/index.android.js
+++ BubbleCounsel/index.android.js
@@ -1,32 +1,19 @@
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Navigator,
-  Text
-} from 'react-native';
-import { Scene, Router, Actions } from 'react-native-router-flux';
-import { Container, Header, Title, Content, Footer, FooterTab, Button, Icon } from 'native-base';
+import { AppRegistry } from 'react-native';
+import { Scene, Router } from 'react-native-router-flux';
 
 import MainView from './app/views/MainView';
 
 export default class BubbleCounsel extends Component {
   render() {
     return (
-      <Router sceneStyle={{backgroundColor:'red'}}>
-        <Scene key="root" hideNavBar={true} style={styles.container}>
+      <Router>
+        <Scene key="root" hideNavBar={true} >
           <Scene key="main" component={MainView} title="Issues" initial={true}  />
         </Scene>
       </Router>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000000'
-  }
-});
 
 AppRegistry.registerComponent('BubbleCounsel', () => BubbleCounsel);
--- BubbleCounsel/index.ios.js
+++ BubbleCounsel/index.ios.js
@@ -1,31 +1,19 @@
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Navigator,
-  Text
-} from 'react-native';
-import { Scene, Router, Actions } from 'react-native-router-flux';
-import { Container, Header, Title, Content, Footer, FooterTab, Button, Icon } from 'native-base';
+import { AppRegistry } from 'react-native';
+import { Scene, Router } from 'react-native-router-flux';
 
 import MainView from './app/views/MainView';
-import ProfileFormView from './app/views/ProfileFormView';
 
 export default class BubbleCounsel extends Component {
   render() {
     return (
       <Router>
-        <Scene key="root" hideNavBar={true}>
+        <Scene key="root" hideNavBar={true} >
           <Scene key="main" component={MainView} title="Issues" initial={true}  />
-          <Scene key="profileForm" component={ProfileFormView} title="Profile Form" />
         </Scene>
       </Router>
     );
   }
 }
-
-const styles = StyleSheet.create({
-
-});
 
 AppRegistry.registerComponent('BubbleCounsel', () => BubbleCounsel);
